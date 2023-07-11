ee602ce448c97abb35b4c7742a7edfa41603b746
Yujanxd <139153799+Yujanxd@users.noreply.github.com>
2023-07-11 14:26:45 -0500

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,15 @@
 alert('javascript cargado')
+$(document).ready(function() {
+    // Realizar solicitud GET a la API
+    $.get("https://sheetdb.io/api/v1/vr0qpo47pi28j", function(data) {
+      // Manipular los datos recibidos de la API
+      var apiData = "";
+      for (var i = 0; i < data.length; i++) {
+        // Construir una cadena con los datos de la API
+        apiData += "<p>" + data[i].NOMBRE + ": " + data[i].APELLIDO + ": " + data[i].TELF + "</p>";
+      }
+      
+      // Mostrar los datos en el div correspondiente
+      $("#apiData").html(apiData);
+    });
+  });
